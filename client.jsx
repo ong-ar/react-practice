62abe27b2ba961cb9c205c29e3b9b549afcfe1d8
ong-ar <reclusis@gmail.com>
2019-06-13 17:39:44 +0900

    test1 component 추가 및 적용

--- client.jsx
+++ client.jsx
@@ -1,2 +1,6 @@
-const React = require('react');
-const ReactDom = require('react-dom');
+const React = require("react");
+const ReactDom = require("react-dom");
+
+const Test1 = require("./test1");
+
+ReactDom.render(<Test1 />, document.querySelector("#root"));
