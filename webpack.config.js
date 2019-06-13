ad6f67bef9cdf9ea29cbe6042a1d16451c4c9539
ong-ar <reclusis@gmail.com>
2019-06-13 17:47:56 +0900

    웹팩 output 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,12 @@
-module.exports = {
+const path = require("path");
 
+module.exports = {
+  name: "react-practice",
+  mode: "development",
+  devtool: "eval",
+  entry: {}, // 입력
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.js"
+  } // 출력
 };
