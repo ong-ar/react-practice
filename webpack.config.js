6c4cae86068e3faf5366e81d0f005ca9db006b82
ong-ar <reclusis@gmail.com>
2019-06-13 18:01:40 +0900

    babel 적용

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,18 @@ module.exports = {
   entry: {
     app: ["./client"]
   }, // 입력
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"]
+        }
+        // jsx 파일은 babel-loader
+      }
+    ]
+  },
   output: {
     path: path.join(__dirname, "dist"),
     filename: "app.js"
